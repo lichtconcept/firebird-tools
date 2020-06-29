3992d521820c8d44c3c7c2d743ff5c431087f468
Johann Neuhauser <johann@it-neuhauser.de>
2020-06-29 14:58:26 +0200

    rework backup script

--- backup_fbdb.sh
+++ backup_fbdb.sh
@@ -3,13 +3,9 @@
 # enable debugging
 #set -x
 
-DB_SERVER="localhost"
-PROD_DATABASE="prod"
-TEST_DATABASE="test"
+PROD_DATABASE_FILE="/var/lib/firebird/3.0/data/prod.fdb"
 TEST_DATABASE_FILE="/var/lib/firebird/3.0/data/test.fdb"
 BACKUP_DIR="/mnt/backups"
-DB_SUFFIX=".fdb"
-BACKUP_SUFFIX=".fbk"
 BACKUP_KEEP_COUNT="20"
 SYSDBA_PASSWORD_FILE="/etc/firebird/3.0/SYSDBA.password"
 ISC_PASSWORD_FILE="$(dirname $0)/ISC_PASSWORD_FILE"
@@ -30,16 +26,23 @@ fi
 
 ####################################################
 
-# get current year, month and timestamp
-DIR_YEAR=$(date +%Y)
-DIR_MONTH=$(date +%m)
-FILE_TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+get_backup_filename() {
+    local DB=$1
+    # remove path
+    DB=${DB##*/}
+    # remove extension
+    DB=${DB%.*}
+    # create backup file dst path
+    echo "${BACKUP_DIR}/$(date +%Y)/$(date +%m)/$(date +%Y-%m-%d_%H-%M-%S)_${DB}.fbk"
+}
+
+PROD_DATABASE_BACKUP_FILE=$(get_backup_filename ${PROD_DATABASE_FILE})
 
 # Start
-echo "Starting Backup of Database ${DB_SERVER}:${PROD_DATABASE}${DB_SUFFIX} at ${FILE_TIMESTAMP}"
+echo "$(date): Starting Backup of Database ${PROD_DATABASE_FILE}"
 
 # create directory if it does not exist
-mkdir -p "${BACKUP_DIR}/${DIR_YEAR}/${DIR_MONTH}"
+mkdir -p "${PROD_DATABASE_BACKUP_FILE%/*}"
 
 # Backup DB from DB-Server
 if gbak \
@@ -47,12 +50,12 @@ if gbak \
     -password "${ISC_PASSWORD}" \
     -backup_database \
     -transportable \
-    "${DB_SERVER}:${PROD_DATABASE}${DB_SUFFIX}" \
-    "${BACKUP_DIR}/${DIR_YEAR}/${DIR_MONTH}/${FILE_TIMESTAMP}_${PROD_DATABASE}${BACKUP_SUFFIX}"
+    "${PROD_DATABASE_FILE}" \
+    "${PROD_DATABASE_BACKUP_FILE}"
 then
-    echo "Successful backed up Database ${DB_SERVER}:${PROD_DATABASE}${DB_SUFFIX}"
+    echo "$(date): Successful backed up Database ${PROD_DATABASE_FILE}"
 else
-    echo "Error while backing up Database ${DB_SERVER}:${PROD_DATABASE}${DB_SUFFIX}"
+    echo "$(date): Error while backing up Database ${PROD_DATABASE_FILE}"
     exit 1
 fi
 
@@ -64,13 +67,13 @@ if [ "$1" = "restore-test-db" ] ; then
         -user "${ISC_USER}" \
         -password "${ISC_PASSWORD}" \
         -recreate_database overwrite \
-        "${BACKUP_DIR}/${DIR_YEAR}/${DIR_MONTH}/${FILE_TIMESTAMP}_${PROD_DATABASE}${BACKUP_SUFFIX}" \
-        "${TEST_DATABASE}${DB_SUFFIX}"
+        "${PROD_DATABASE_BACKUP_FILE}" \
+        "${TEST_DATABASE_FILE}"
     then
         chown firebird:firebird ${TEST_DATABASE_FILE}
-        echo "Successful restored Database ${DB_SERVER}:${PROD_DATABASE}${DB_SUFFIX} to ${DB_SERVER}:${TEST_DATABASE}${DB_SUFFIX}"
+        echo "$(date): Successful restored Database ${PROD_DATABASE_BACKUP_FILE} to ${TEST_DATABASE_FILE}"
     else
-        echo "Error while restoring Database ${DB_SERVER}:${PROD_DATABASE}${DB_SUFFIX} to ${DB_SERVER}:${TEST_DATABASE}${DB_SUFFIX}"
+        echo "$(date): Error while restoring Database ${PROD_DATABASE_BACKUP_FILE} to ${TEST_DATABASE_FILE}"
         exit 1
     fi
 fi
@@ -78,9 +81,9 @@ fi
 # Remove old backups
 for fbk in $(find ${BACKUP_DIR} -type f -iname "*.fbk" | sort | head -n -${BACKUP_KEEP_COUNT})
 do
-    echo "Removing: ${fbk}"
+    echo "$(date): Removing: ${fbk}"
     rm "${fbk}"
 done
 
-echo "Finished Backup of Database ${DB_SERVER}:${PROD_DATABASE}${DB_SUFFIX} at $(date +%Y-%m-%d_%H-%M-%S)"
+echo "$(date): Finished Backup of Database ${PROD_DATABASE_FILE}"
 exit 0
